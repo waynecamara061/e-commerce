44f1c1fdbb2d3c27e7caa3bd2d1a539f62d129ed
Emma paris <waynecamara@biud.com.br>
2023-03-25 15:10:42 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  // {
+  //   path: "/home",
+  //   element: <Home />,
+  // },
+  // {
+  //   path: "/products",
+  //   element: <App />,
+  // },
+  // {
+  //   path: "/pay",
+  //   element: <App />,
+  // },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
